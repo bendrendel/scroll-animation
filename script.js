2f979beb27bbe2c6a4717f425569ed42cc60a947
bendrendel <bfdrendel@gmail.com>
2021-01-29 16:16:30 -0700

    Formatted files with prettier

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-const boxes = document.querySelectorAll('.box');
+const boxes = document.querySelectorAll(".box");
 
-window.addEventListener('scroll', checkBoxes);
+window.addEventListener("scroll", checkBoxes);
 
 checkBoxes();
 
 function checkBoxes() {
-    const triggerBottom = window.innerHeight * 0.9;
-    let boxNumber = 1;
+  const triggerBottom = window.innerHeight * 0.9;
+  let boxNumber = 1;
 
-    console.clear();
-    console.log(`Window Height: ${triggerBottom}`);
+  console.clear();
+  console.log(`Window Height: ${triggerBottom}`);
 
-    boxes.forEach(box => {
-        const boxTop = box.getBoundingClientRect().top;
-        
-        console.log(`Box ${boxNumber} Top: ${boxTop} ${boxTop < triggerBottom}`);
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
 
-        if (boxTop < triggerBottom) {
-            box.classList.add('show');
-        } else {
-            box.classList.remove('show');
-        }
-        
-        boxNumber++;
-    })
+    console.log(`Box ${boxNumber} Top: ${boxTop} ${boxTop < triggerBottom}`);
+
+    if (boxTop < triggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+
+    boxNumber++;
+  });
 }
